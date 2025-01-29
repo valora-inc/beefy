fc811bb290dc87b8e3012073f1419dd60ebbe641
Kathy Luo <kathyluo18@gmail.com>
2025-01-29 17:25:32 +0100

    fix: tweaks

--- babel.config.js
+++ babel.config.js
@@ -10,10 +10,10 @@ module.exports = function (api) {
             crypto: 'react-native-quick-crypto',
             stream: 'readable-stream',
             buffer: '@craftzdog/react-native-buffer',
-            // Temporary hack so we can directly import from the @interaxyz/mobile package
-            // without transpiling, we'll probably add a custom preset for this
-            '^src/(.+)$': '../../node_modules/@interaxyz/mobile/src/\\1',
-            '^locales$': '../../node_modules/@interaxyz/mobile/locales',
+            // Hack so we can directly import from the @mobilestack/runtime package
+            // without transpiling
+            '^src/(.+)$': './node_modules/@interaxyz/mobile/src/\\1',
+            '^locales$': './node_modules/@interaxyz/mobile/locales',
           },
         },
       ],
