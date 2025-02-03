5c604766841ebbd7afb566c4237bd4d3bd24512e
Finnian Jacobson-Schulte <140328381+finnian0826@users.noreply.github.com>
2025-02-04 04:57:49 +1300

    feat: Use earn tab and set as initial screen (#5)
    * feat: Use earn tab and set as initial screen
    
    * change tab name to home
    
    * Remove todo
    
    Co-authored-by: Jean Regisser <jean.regisser@gmail.com>
    
    * add secrets todo
    
    Co-authored-by: Jean Regisser <jean.regisser@gmail.com>
    
    ---------
    
    Co-authored-by: Jean Regisser <jean.regisser@gmail.com>

--- index.ts
+++ index.ts
@@ -22,19 +22,27 @@ const App = createApp({
   },
   features: {
     cloudBackup: true,
+    // TODO: pass these using expo public env variables
+    statsig: {
+      apiKey: 'statsig-api-key'
+    },
+    segment: {
+      apiKey: 'segment-api-key'
+    }
   },
   screens: {
     tabs: ({ defaultTabs }) => {
       return {
-        // TODO: add earn tab and initial route to it
         screens: [
           defaultTabs.wallet,
+          {...defaultTabs.earn, label: (t) => t('home')},
           {
             ...defaultTabs.activity,
             label: (t) => t('activity'),
             icon: ActivityIcon,
           },
         ],
+        initialScreen: 'earn',
       }
     },
   },
