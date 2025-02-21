b11c14706996e26e6189f61bcf22bcbe0494964f
Jean Regisser <jean.regisser@gmail.com>
2025-02-21 11:46:55 +0100

    chore: update to latest @divvi/mobile

--- babel.config.js
+++ babel.config.js
@@ -3,18 +3,6 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      [
-        'module-resolver',
-        {
-          alias: {
-            crypto: 'react-native-quick-crypto',
-            stream: 'readable-stream',
-            buffer: '@craftzdog/react-native-buffer',
-            '^src/(.+)$': './node_modules/@divvi/mobile/src/\\1',
-            '^locales$': './node_modules/@divvi/mobile/locales',
-          },
-        },
-      ],
       'react-native-reanimated/plugin', // NOTE: this plugin MUST be last
     ],
   }
